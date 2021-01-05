8ec2260d1233e0aa0aa1f559218dd3910caccb43
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-01-05 14:04:49 +0530

    15th Project 31-12-2020

---
+++ 15 - IOCProj15-CollectionInjection-Properties/src/com/nt/test/CollectionInjectionTest.java
@@ -0,0 +1,21 @@
+package com.nt.test;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import com.nt.beans.CourseInfo;
+
+public class CollectionInjectionTest {
+
+	public static void main(String[] args) {
+		//create IOC container
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
+		//create spring bean obj
+		CourseInfo info = factory.getBean("info",CourseInfo.class);
+		System.out.println(info);
+
+	}
+
+}
