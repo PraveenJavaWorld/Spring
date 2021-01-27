bcfc3f249f92fecb83ee46ba3037a5f59c6d8204
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-01-27 17:39:34 +0530

    22nd project 09-01-2021

---
+++ 22 - IOCProj22-StrategyPattern-Spring-AutoWiring-byName/src/com/nt/comp/PetrolEngine.java
@@ -0,0 +1,17 @@
+package com.nt.comp;
+
+public class PetrolEngine implements IEngine {
+
+	@Override
+	public void start() {
+		System.out.println("Petrol Engine started---");
+
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Petrol Engine stopped---");
+
+	}
+
+}
