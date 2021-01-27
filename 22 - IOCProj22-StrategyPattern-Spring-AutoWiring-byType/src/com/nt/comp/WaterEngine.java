72085d6b4de7e5633a6328d9bc3876c2df976fdb
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-01-27 19:32:50 +0530

    22nd project 09-01-2021

---
+++ 22 - IOCProj22-StrategyPattern-Spring-AutoWiring-byType/src/com/nt/comp/WaterEngine.java
@@ -0,0 +1,17 @@
+package com.nt.comp;
+
+public class WaterEngine implements IEngine {
+
+	@Override
+	public void start() {
+		System.out.println("Water Engine started---");
+
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Water Engine stopped---");
+
+	}
+
+}
