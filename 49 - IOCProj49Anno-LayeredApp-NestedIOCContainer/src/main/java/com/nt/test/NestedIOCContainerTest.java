05d485b6076760343e38394089e231cd1d2abe3f
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-02-25 17:04:03 +0530

    49th Project 20-02-2021

---
+++ 49 - IOCProj49Anno-LayeredApp-NestedIOCContainer/src/main/java/com/nt/test/NestedIOCContainerTest.java
@@ -0,0 +1,33 @@
+package com.nt.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.controller.MainController;
+
+public class NestedIOCContainerTest {
+
+	public static void main(String[] args) {
+		
+		//create Parent IOC Container
+		ClassPathXmlApplicationContext pCtx = new ClassPathXmlApplicationContext("com/nt/cfgs/business-beans.xml");
+		//create Child IOC Container
+		ClassPathXmlApplicationContext cCtx = new ClassPathXmlApplicationContext(new String[] {"com/nt/cfgs/presentation-beans.xml"},pCtx);
+		
+		//get Controller Obj
+		MainController controller = cCtx.getBean("controller",MainController.class);
+		//invoke methods
+		try {
+			controller.showEmpsByDesgs("CLERK", "MANAGER", "SALESMAN").forEach(dto->{
+				System.out.println(dto);
+			});
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//close Containers
+		pCtx.close();
+		cCtx.close();
+
+	}//main
+
+}
