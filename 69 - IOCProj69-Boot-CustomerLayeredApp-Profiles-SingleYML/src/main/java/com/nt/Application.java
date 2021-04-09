d6a3fae95562038b56e4ffd96c15c61fb2c626e1
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-04-09 23:01:13 +0530

    69th Project 22-03-2021

---
+++ 69 - IOCProj69-Boot-CustomerLayeredApp-Profiles-SingleYML/src/main/java/com/nt/Application.java
@@ -0,0 +1,62 @@
+package com.nt;
+
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.nt.controller.BankController;
+import com.nt.vo.CustomerVO;
+
+@SpringBootApplication
+public class Application {
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	@Profile("uat")
+	public DataSource createDS() throws Exception {
+		ComboPooledDataSource ds = new ComboPooledDataSource();
+		ds.setDriverClass(env.getRequiredProperty("spring.datasource.driver-class-name"));
+		ds.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+		ds.setUser(env.getRequiredProperty("spring.datasource.username"));
+		ds.setPassword(env.getRequiredProperty("spring.datasource.password"));
+		return ds;
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		
+		try {
+			//get Spring bean class obj
+			BankController controller = ctx.getBean("controller",BankController.class);
+			//prepare VO obj
+			CustomerVO vo = new CustomerVO();
+			vo.setCname("PRAVEEN");
+			vo.setAddress("HYD");
+			vo.setPAmt("100000");
+			vo.setRate("2");
+			vo.setTime("24");
+			
+			//invoke b.method
+			String result = controller.processCustomer(vo);
+			System.out.println("Interest Amount is :: "+result);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Activated Profile is :: "+Arrays.toString(ctx.getEnvironment().getActiveProfiles()));
+		//close container
+		((ConfigurableApplicationContext) ctx).close();
+	}
+
+}
