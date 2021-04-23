d2ce43e4a35e9d1cad22918af41abfeee82447ee
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-04-23 23:25:44 +0530

    82nd Project 23-04-2021

---
+++ 82 - AOPProj01-AroundAdvice-DeclarativeApproach/src/main/java/com/nt/service/BankService.java
@@ -0,0 +1,15 @@
+package com.nt.service;
+
+public class BankService {
+	
+	public double calSimpleInterest(float pAmt,float rate,float time) {
+		System.out.println("BankService.calSimpleInterest()");
+		return (pAmt*rate*time)/100.0f;
+	}
+	
+	public double calCompoundInterest(float pAmt,float rate,float time) {
+		System.out.println("BankService.calCompoundInterest()");
+		return (pAmt*Math.pow(1+rate/100, time))-pAmt;
+	}
+
+}
---
+++ 82 - AOPProj01-AroundAdvice-DeclarativeApproach/src/main/java/com/nt/test/AspectJAOPAroundAdviceTest.java
@@ -0,0 +1,28 @@
+package com.nt.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.service.BankService;
+
+public class AspectJAOPAroundAdviceTest {
+
+	public static void main(String[] args) {
+		
+		//create IOC Container
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+		//get Proxy class obj
+		BankService proxy = ctx.getBean("service", BankService.class);
+		System.out.println(proxy.getClass()+" "+proxy.getClass().getSuperclass());
+		//invoke b.methods
+		System.out.println(proxy.calSimpleInterest(20000, 10, 12));
+		System.out.println("=====================");
+		System.out.println(proxy.calCompoundInterest(20000, 10, 12));
+		
+		//close container
+		((AbstractApplicationContext) ctx).close();
+
+	}
+
+}
