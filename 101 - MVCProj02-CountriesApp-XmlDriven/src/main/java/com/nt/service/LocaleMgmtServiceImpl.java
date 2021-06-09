1d4bb28534647975d99da27132ec2cac1eeb81a7
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-06-09 14:13:24 +0530

    101th Project 08-06-2021

---
+++ 101 - MVCProj02-CountriesApp-XmlDriven/src/main/java/com/nt/service/LocaleMgmtServiceImpl.java
@@ -0,0 +1,21 @@
+package com.nt.service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+public class LocaleMgmtServiceImpl implements ILocaleMgmtService {
+
+	@Override
+	public List<String> getCountriesList() {
+		Locale locale[] = Locale.getAvailableLocales();
+		List<String> list = new LinkedList<>();
+		for(Locale l:locale) {
+			if(l!=null && l.getDisplayCountry().length()!=0) {
+				list.add(l.getDisplayCountry());
+			}
+		}
+		return list;
+	}
+
+}
---
+++ 101 - MVCProj02-CountriesApp-XmlDriven/src/main/webapp/WEB-INF/pages/home.jsp
@@ -0,0 +1,6 @@
+
+
+
+<h1 style="color: red;text-align: center;">WELCOME TO SPRING MVC</h1>
+<br>
+<h2 style="color: green;text-align: center;"><a href="countriesList.do">Get_Countries_List</a></h2>
